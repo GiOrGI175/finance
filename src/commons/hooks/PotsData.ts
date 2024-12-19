f7973d4b822cf4f481c61e8d3bf85bfadd02daf3
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-12-20 00:49:23 +0400

    feature/

---
+++ src/commons/hooks/PotsData.ts
@@ -0,0 +1,21 @@
+export type Pots = {
+  method: string;
+  sum: number;
+  procent: number;
+  limit: number;
+};
+
+export const Potss: Pots[] = [
+  {
+    method: 'Savings',
+    sum: 159.0,
+    procent: 7.95,
+    limit: 2000,
+  },
+  {
+    method: 'Savings',
+    sum: 110.0,
+    procent: 99,
+    limit: 2000,
+  },
+];
