f21ba854b1895a002279afe4a3534c759401f544
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-12-25 18:52:20 +0400

    fix/Navbar
    fixing navbar animation

--- src/components/__molecules/NavBar.tsx
+++ src/components/__molecules/NavBar.tsx
@@ -30,13 +30,13 @@ const NavBar = () => {
 
   return (
     <div
-      className={`w-[300px] min-h-screen pt-[32px]  pr-[24px] bg-[#201F24] rounded-tr-[25px] rounded-br-[25px]  flex flex-col justify-between duration-1000 overflow-hidden ${
-        popUpNavBar && 'w-[88px] pl-[0px] pr-[0px] pt-[40px]'
+      className={`w-[300px] min-h-screen pt-[32px]  bg-[#201F24] rounded-tr-[25px] rounded-br-[25px]  flex flex-col justify-between duration-1000 overflow-hidden ${
+        popUpNavBar && 'w-[88px] !px-[0px] !pr-[0px] items-center '
       }  max-md:w-full max-md:min-h-fit max-md:h-[74px] max-md:rounded-br-[0px] max-md:rounded-tr-[8px] max-md:rounded-tl-[8px] max-md:pl-[0px] max-md:pr-[0px] max-md:pt-[10px]  max-sm:h-[52px] `}
     >
       <div>
         <div
-          className={`w-full mb-[64px] flex duration-1000 ${
+          className={`w-full mb-[64px] flex ${
             popUpNavBar && 'justify-center'
           }  max-md:hidden`}
         >
@@ -45,7 +45,7 @@ const NavBar = () => {
             width={popUpNavBar ? 16 : 121}
             height={21}
             alt='logo'
-            className='ml-[32px] duration-1000'
+            className={`${popUpNavBar ? 'ml-[0px] ' : 'ml-[32px]'}  `}
           />
         </div>
         <nav>
@@ -55,8 +55,8 @@ const NavBar = () => {
       </div>
 
       <button
-        className={`mb-[128px] ml-[32px] w-full flex items-center   ${
-          popUpNavBar && 'justify-center ml-[0px]'
+        className={` mb-[128px] ml-[32px] w-full flex items-center   ${
+          popUpNavBar && 'ml-[0px] justify-center'
         } max-md:hidden group `}
         onMouseMove={() => setArrowhover(true)}
         onMouseLeave={() => setArrowhover(false)}
@@ -67,7 +67,9 @@ const NavBar = () => {
           width={24}
           height={24}
           alt='icon'
-          className={`mr-[16px] ${popUpNavBar && 'mr-[0px] rotate-180'}`}
+          className={`mr-[16px] duration-1000 ${
+            popUpNavBar && 'mr-[0px] rotate-180 '
+          }`}
         />
         <span
           className={`duration-1000 font-publicSans font-bold leading-[24px] text-[#B3B3B3] text-[19px] group-hover:text-[#F2F2F2]  ${
--- src/components/__molecules/NavLinks.tsx
+++ src/components/__molecules/NavLinks.tsx
@@ -13,27 +13,23 @@ const NavLinksArr: React.FC<PopUpNavBarProps> = ({ popUpNavBar }) => {
   const PathName = usePathname();
 
   return (
-    <ul
-      className={`duration-1000 ${
-        popUpNavBar && 'w-full flex flex-col justify-center items-center'
-      }   max-md:hidden `}
-    >
+    <ul className={`duration-1000   max-md:hidden `}>
       {NavLinks.map((item) => {
         const isActive = PathName.startsWith(item.link);
         return (
           <li
             key={item.name}
-            className={`mb-[4px] pl-[32px] flex duration-1000  ${
+            className={`max-w-[276px] min-w-[56px] w-full mb-[4px] pl-[32px] flex duration-1000  ${
               isActive && 'bg-white border-l-[4px] border-[#277C78] '
             } rounded-tr-[12px] rounded-br-[12px]  ${
               popUpNavBar &&
-              'rounded-[50%] w-[56px] h-[56px] rounded-tr-[50%] rounded-br-[50%] justify-center items-center !border-[0px]  !pl-[0px]'
+              '  !rounded-full !border-[0px]  !pl-[0px] justify-center items-center '
             }`}
           >
             <Link
               href={item.link}
               className={`w-full h-[56px] duration-1000  flex items-center ${
-                popUpNavBar && 'justify-center'
+                popUpNavBar && 'justify-center items-center'
               }  group`}
             >
               <Image
@@ -41,7 +37,7 @@ const NavLinksArr: React.FC<PopUpNavBarProps> = ({ popUpNavBar }) => {
                 width={25}
                 height={25}
                 alt='icon'
-                className={`mr-[16px]  ${popUpNavBar && 'mr-[0px]'}`}
+                className={`mr-[16px]  ${popUpNavBar && '!mr-[0px]'}`}
               />
               <span
                 className={`font-publicSans font-bold leading-[24px] group-hover:text-[#F2F2F2] duration-1000  ${
