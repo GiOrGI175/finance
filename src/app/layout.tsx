c711cfada858c6fda772b56523d80c9d77ac7174
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-12-27 14:29:20 +0400

    fix/navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang='en' className='flex justify-center'>
       <body className='flex max-w-[3000px] w-full min-h-screen bg-[#F8F4F0] max-md:flex-col-reverse '>
-        <div className='w-fit max-md:max-w-[1024px]: max-md:w-full max-md:fixed'>
+        <div className='w-fit max-md:max-w-[1024px]  max-md:w-full max-md:fixed'>
           {!disableNavWithFooter.includes(path) && <NavBar />}
         </div>
         <div className='w-full '>{children}</div>
