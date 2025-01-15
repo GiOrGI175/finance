81901cddf37cd348327066f7d2aa60de51da015a
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-01-15 18:19:23 +0400

    feature/fetch

--- src/components/__organisms/PotsPage.tsx
+++ src/components/__organisms/PotsPage.tsx
@@ -10,8 +10,46 @@ import DeletePot from '../__molecules/DeletePot';
 import MoneyTransferBtn from '../__atoms/MoneyTransferBtn';
 import WithdrawPot from '../__molecules/WithdrawPot';
 import AddMoneyPot from '../__molecules/AddMoneyPot';
+import { useEffect, useState } from 'react';
+import axiosInstance from '@/commons/hooks/lib/axiosInstance';
+
+type PotT = {
+  _id: string;
+  potName: string;
+  Target: number;
+  theme: string;
+  __v: number;
+  Amount: number;
+};
 
 const PotsPage = () => {
+  const [potsData, setPostsData] = useState<PotT[]>([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const res = await axiosInstance.get('/pots');
+      setPostsData(res.data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className='w-full overflow-x-hidden overflow-scroll h-screen py-[40px]  px-[40px] bg-[#F8F4F0] max-sm:px-[16px] max-sm:py-[24px]'>
       <div className='w-full mb-[32px] flex justify-between'>
@@ -23,16 +61,16 @@ const PotsPage = () => {
         </>
       </div>
       <div className='w-full flex flex-wrap justify-center gap-[24px]'>
-        {Potss.map((item, index) => (
+        {potsData.map((item, index) => (
           <div
-            key={item.id}
+            key={item._id}
             className='basis-[518px]  grow w-full h-[303px] rounded-[12px] flex flex-col justify-between p-[24px] bg-white max-sm:p-[20px]'
           >
             <div className='flex justify-between mb-[32px]'>
               <div className='flex'>
                 <Image src={item.icon} width={16} height={16} alt='icon' />
                 <span className='ml-[16px] font-publicSans font-bold text-[20px] leading-[24px] text-[#201F24]'>
-                  {item.method}
+                  {item.potName}
                 </span>
               </div>
               <>
@@ -48,7 +86,7 @@ const PotsPage = () => {
                 </div>
                 <div>
                   <span className='font-publicSans font-bold text-[32px] leading-[38px] text-[#201F24]'>
-                    ${item.sum}
+                    ${item.Amount}
                   </span>
                 </div>
               </div>
@@ -70,7 +108,7 @@ const PotsPage = () => {
                   </div>
                   <div>
                     <span className='font-publicSans font-normal text-[12px] leading-[18px] text-[#696868]'>
-                      Target pf ${item.limit}
+                      Target pf ${item.Target}
                     </span>
                   </div>
                 </div>
